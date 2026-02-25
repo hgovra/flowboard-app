6844b6b824e210639421c362f84d750a2deebc0e
Hugo Vieira <hgovra@gmail.com>
2026-02-25 16:38:27 -0300

    chore(lint): configure angular eslint selectors to use fb prefix

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'fb',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'fb',
           style: 'kebab-case',
         },
       ],
--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { NxWelcome } from './nx-welcome';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  selector: 'fb-root',
+  imports: [RouterOutlet],
+  template: `<router-outlet />`,
 })
-export class App {
-  protected title = 'web';
-}
+export class AppComponent {}
